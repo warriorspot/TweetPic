165523dc9ddb994f629afc42a423911490cd2386
Brennan Cleveland <warriorspot@gmail.com>
2012-07-20 21:28:19 -0500

    initial implementation of TweetRequest

--- TweetPic/Networking/TweetRequest.h
+++ TweetPic/Networking/TweetRequest.h
@@ -1,6 +1,14 @@
 
 #import <Foundation/Foundation.h>
+#import <Twitter/Twitter.h>
+#import "Request.h"
 
-@interface TweetRequest : NSObject
+@interface TweetRequest : Request
+
+@property (nonatomic, strong) TWRequest *request;
+
+- (void) startWithSearchTerm: (NSString *) searchTerm;
 
 @end
+
+extern NSString * const TwitterURL;
--- TweetPic/Networking/TweetRequest.m
+++ TweetPic/Networking/TweetRequest.m
@@ -3,4 +3,61 @@
 
 @implementation TweetRequest
 
+@synthesize active;
+@synthesize request;
+
+- (void) start
+{
+    [self startWithSearchTerm:nil];
+}
+
+- (void) startWithSearchTerm:(NSString *)searchTerm
+{
+    if(searchTerm == nil || [searchTerm length] == 0)
+    {
+        return;
+    }
+    
+    active = YES;
+    
+    NSString *term = [searchTerm stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *urlString = [NSString stringWithFormat:TwitterURL, term];
+    NSURL *url = [NSURL URLWithString: urlString];
+    
+    request = [[TWRequest alloc] initWithURL: url
+                                  parameters: nil 
+                               requestMethod: TWRequestMethodGET];
+    
+    [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error)
+    {
+        active = NO;
+        
+        if ([urlResponse statusCode] == 200) 
+        {
+            NSError *error;        
+            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:responseData 
+                                                                 options:0 
+                                                                   error:&error];
+            NSLog(@"Twitter response: %@", json); 
+            
+            if(self.delegate && [self.delegate respondsToSelector:@selector(request:didSucceed:)])
+            {
+                [self.delegate request:self didSucceed:json];                    
+            }
+        }
+        else
+        {
+            NSLog(@"Twitter error, HTTP response: %i", [urlResponse statusCode]);
+            
+            if(self.delegate && [self.delegate respondsToSelector:@selector(request:didFailWithError:)])
+            {
+                [self.delegate request:self didFailWithError:error];                    
+            }
+        }
+    }];
+}
+
 @end
+
+NSString * const TwitterURL = @"http://search.twitter.com/search.json?q=%@&rpp=100&result_type=recent";
+
