829f1778e59b7f59f7c6bf96a5243f7c215c246e
Brennan Cleveland <warriorspot@gmail.com>
2012-07-20 22:37:34 -0500

    Complete implementation of TweetPic class

--- TweetPic/Models/TweetPic.h
+++ TweetPic/Models/TweetPic.h
@@ -3,4 +3,10 @@
 
 @interface TweetPic : NSObject
 
+@property (nonatomic, strong) NSString *tweet;
+@property (nonatomic, strong) UIImage *image;
+
+- (id) initWithTweet: (NSString *) tweet image: (UIImage *) image;
+
+
 @end
--- TweetPic/Models/TweetPic.m
+++ TweetPic/Models/TweetPic.m
@@ -3,4 +3,24 @@
 
 @implementation TweetPic
 
+@synthesize image;
+@synthesize tweet;
+
+- (id) init
+{
+    return [self initWithTweet:@"Hello, World!" image:[UIImage imageNamed:@"beer.jpg"]];
+}
+
+- (id) initWithTweet:(NSString *)newTweet image:(UIImage *)newImage
+{
+    self = [super init];
+    if(self)
+    {
+        image = newImage;
+        tweet = newTweet;
+    }
+    
+    return self;
+}
+
 @end
