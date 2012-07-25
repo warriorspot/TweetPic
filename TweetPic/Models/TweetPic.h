bbd1a29e9b0fdf4c6396db6d53b2a83a74fe0b59
Brennan Cleveland <brennan@livefront.com>
2012-07-24 21:23:27 -0500

    inline documentation

--- TweetPic/Models/TweetPic.h
+++ TweetPic/Models/TweetPic.h
@@ -1,11 +1,18 @@
 
 #import <Foundation/Foundation.h>
 
+/// A TweetPic object pairs a tweet with an image.
+///
 @interface TweetPic : NSObject
 
+/// The tweet
 @property (nonatomic, strong) NSString *tweet;
+
+/// The image
 @property (nonatomic, strong) UIImage *image;
 
+/// Designated initializer
+///
 - (id) initWithTweet: (NSString *) tweet image: (UIImage *) image;
 
 
