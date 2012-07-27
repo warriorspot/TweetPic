0222f851b159c080636e7f21a6d24bcda845ff0f
Brennan Cleveland <brennan@livefront.com>
2012-07-26 21:55:19 -0500

    Implement Movie model object

--- TweetPic/Models/Movie.h
+++ TweetPic/Models/Movie.h
@@ -3,4 +3,7 @@
 
 @interface Movie : NSObject
 
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSURL *imageURL;
+
 @end
--- TweetPic/Models/Movie.m
+++ TweetPic/Models/Movie.m
@@ -3,4 +3,15 @@
 
 @implementation Movie
 
+@synthesize imageURL;
+@synthesize title;
+
+
+#pragma mark - instance methods
+
+- (NSString *) description
+{
+    return [NSString stringWithFormat:@"title: %@ posters: %@", self.title, [self.imageURL absoluteString]];
+}
+
 @end
