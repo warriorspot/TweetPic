5b8a22cedf8a646aee0b94704fb1c74f885d344b
Brennan Cleveland <warriorspot@gmail.com>
2012-07-28 13:46:56 -0500

    remove separate private interface file

--- TweetPic/Controllers/TweetPicViewController.m
+++ TweetPic/Controllers/TweetPicViewController.m
@@ -4,7 +4,33 @@
 #import "TweetPicCell.h"
 #import "TweetPicManager.h"
 #import "TweetPicViewController.h"
-#import "TweetPicViewController+Private.h"
+
+
+@interface TweetPicViewController()
+
+@property (nonatomic, assign) IBOutlet UIButton *dismissKeyboardButton;
+@property NSUInteger tweetPicCount;
+
+- (IBAction) dismissKeyboard:(id)sender;
+
+- (IBAction) sortTweetPics:(id)sender;
+
+- (void) keyboardDidHide: (NSNotification *) notification;
+
+- (void) keyboardDidShow: (NSNotification *) notification;
+
+- (void) showMessageWithTitle: (NSString *) title message: (NSString *) message;
+
+- (void) toggleView: (UIView *) view visible: (BOOL) isVisible animated: (BOOL) animated;
+
+- (void) tweetPicErrorNotification: (NSNotification *) notification;
+
+- (void) tweetPicNotification: (NSNotification *) notification;
+
+- (void) tweetPicsCreatedNotification: (NSNotification *) notification;
+
+@end
+
 
 @implementation TweetPicViewController
 
