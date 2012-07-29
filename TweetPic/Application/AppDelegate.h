0a34ed4be43b4a33efb1cc4baa3f12041821d40e
Brennan Cleveland <warriorspot@gmail.com>
2012-07-29 13:13:36 -0500

    inline documentation

--- TweetPic/Application/AppDelegate.h
+++ TweetPic/Application/AppDelegate.h
@@ -3,9 +3,17 @@
 
 @class TweetPicManager;
 
+/// AppDelegate for TweetPic.  Creates the initial TweetPicViewController
+/// and sets it as the root view controller.  Creates an instance of
+/// TweetPicManager to respond to the user entering a Twitter search term.
+///
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+/// The main application window
 @property (strong, nonatomic) UIWindow *window;
+
+/// This instance is responsible for querying Twitter and RottenTomatoes
+/// and generating TweetPic instances.
 @property (nonatomic, strong) TweetPicManager *tweetPicManager;
 
 @end
