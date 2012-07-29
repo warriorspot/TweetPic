64e2e8da1932b3c830650c925e62065731ebe1a8
Brennan Cleveland <warriorspot@gmail.com>
2012-07-29 13:57:57 -0500

    inline documentation

--- TweetPic/Models/Movie.h
+++ TweetPic/Models/Movie.h
@@ -1,9 +1,16 @@
 
 #import <Foundation/Foundation.h>
 
+/// Encapsulates the bare minimum of data returned by a request
+/// to the RottenTomatoes API for a given movie needed to create
+/// a TweetPic instance.
+///
 @interface Movie : NSObject
 
+/// The title of the movie.
 @property (nonatomic, strong) NSString *title;
+
+/// The URL to a thumbnail image of the movie poster.
 @property (nonatomic, strong) NSURL *imageURL;
 
 @end
