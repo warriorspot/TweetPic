0bc18bfea6199b83376fe833d25cc262974e1300
Brennan Cleveland <warriorspot@gmail.com>
2012-07-29 20:33:32 -0500

    inline documentation

--- TweetPic/Networking/TweetRequest.h
+++ TweetPic/Networking/TweetRequest.h
@@ -3,10 +3,20 @@
 #import <Twitter/Twitter.h>
 #import "Request.h"
 
+/// TweetRequest requests tweets from the Twitter API based on a supplied
+/// keyword.  If successful, returns a populated Tweet instance via
+/// the request:didSucceedWithObject: delegate method.
+///
 @interface TweetRequest : Request
 
+/// The specific request used to access the Twitter API
 @property (nonatomic, strong) TWRequest *request;
 
+/// Starts a new request for tweet that contain the given search term.
+///
+/// @param searchTerm
+///     the text used to match tweets via the Twitter API
+///
 - (void) startWithSearchTerm: (NSString *) searchTerm;
 
 @end
